348c5b4435d73449317418bbcae4c88c5bbe20c0
BugHunter-X <askzenai@gmail.com>
2025-02-03 21:26:05 +0100

    Add BugSolution.cs file

---
+++ BugSolution.cs
@@ -0,0 +1,9 @@
+public class ExampleClass
+{
+    public int MyProperty { get; set; } = 0; // Initialized to 0
+
+    public void MyMethod()
+    {
+        Console.WriteLine(MyProperty.ToString());
+    }
+}
